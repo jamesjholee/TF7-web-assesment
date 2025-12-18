2a90840343e7f7fc88281e560d6a430a442770b2
jamesjholee <james.lee4789@gmail.com>
2025-12-18 17:27:08 -0500

    Task 2: add active class on hover to give same effect when selected.

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = ({category, setCategory}) => {
+  const [hoveredItem, setHoveredItem] = useState(null);
 
   return (
     <div className='explore-menu' id='explore-menu'>
@@ -11,8 +12,14 @@ const ExploreMenu = ({category, setCategory}) => {
         <div className="explore-menu-list">
             {menu_list.map((item,index)=>{
                 return (
-                    <div onClick={()=>setCategory(prev=> prev === item.menu_name ? 'All' : item.menu_name)} key={index} className="explore-menu-list-item">
-                        <img className={category===item.menu_name?'active':''} src={item.menu_image} alt="" />
+                    <div
+                        onClick={()=>setCategory(prev=> prev === item.menu_name ? 'All' : item.menu_name)}
+                        onMouseEnter={()=>setHoveredItem(item.menu_name)}
+                        onMouseLeave={()=>setHoveredItem(null)}
+                        key={index}
+                        className="explore-menu-list-item"
+                    >
+                        <img className={category===item.menu_name || hoveredItem===item.menu_name?'active':''} src={item.menu_image} alt="" />
                         <p>{item.menu_name}</p>
                     </div>
                 )
